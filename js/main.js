367f6283d7dff488a1763999b334ed248c3a7186
Yuka Ishii <ishi0026@algonquinlive.com>
2023-07-07 12:42:31 -0400

    add a click event in image

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ title.addEventListener("click", function() {
     // console.log(this) may not work in responsive view in inspect
     this.style.color = "darkgreen"
 })
+
+const like = document.getElementById("like")
+
+like.addEventListener('click', function(e) {
+    e.target.classList.toggle("liked")
+})
