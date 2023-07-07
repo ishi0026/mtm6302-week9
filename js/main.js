54b05190ffec02d6289e681027fb644b5796cae1
Yuka Ishii <ishi0026@algonquinlive.com>
2023-07-07 12:38:18 -0400

    add addEventListener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 console.log("JS file included")
+
+const title = document.querySelector("h1")
+
+title.addEventListener("click", function() {
+    // console.log(this) may not work in responsive view in inspect
+    this.style.color = "darkgreen"
+})
